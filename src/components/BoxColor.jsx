89d4c334819a97534f37359504ccf904f2a7f402
AnaBMo <anab_mj@outlook.com>
2025-04-02 21:21:35 +0200

    ejercicio terminado

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,31 @@
-function boxColor ({color, value}) {
+/* 
+Aquí irá el componente del cuadrado de color.
+*/
+import { useState, useRef, useEffect } from 'react';
+
+function BoxColor({ color, value }) {
+
+  const [classList, setClassList] = useState('');
+  const boxRef = useRef(null);
+
+  const boxStyle = {
+    backgroundColor: classList.includes(`box${value}`) ? color : null,
+    border: `2px solid ${color}`,
+    color: color
+  };
+
+  useEffect(() => {
+    setClassList(boxRef.current.className)
+  }, []);
+
   return (
     <>
-    </> 
+      <div ref={boxRef} className={color === value ? "yes" : "not"} style={boxStyle}>
+        <h3 >
+          {color === value ? `Yes, I am ${value} color` : `I am not a ${value} color`}
+        </h3>
+      </div>
+    </>
   )
 }
-export default boxColor;  
+export default BoxColor;  
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,33 @@
+/* 
+Aquí crearemos el formulario e importaremos las BoxColor para pasarle props. 
+*/
 
+import { useRef, useState } from 'react';
+import BoxColor from './BoxColor'
 
 function MyForm() {
 
+  const inputRef = useRef(null);
+  const [value, setValue] = useState('');
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+
   return (
-    <></>
+    <>
+      <section>
+        <input
+          type="text"
+          placeholder="escribe un color"
+          onChange={() => setValue(inputRef.current.value)}
+          value={value}
+          ref={inputRef}
+        />
+        <div className='container'> 
+          { colors.map((color, index) => {
+            return (<BoxColor key={index} color={color} value={value} />)
+          }) } 
+        </div>
+      </section>
+    </>
   );
 }
 
